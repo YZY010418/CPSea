7e6bd4148fd0faba4b4295f328e22d0d8b8a9c80
Yang Ziyi <yangzy23@mails.tsinghua.edu.cn>
2025-10-11 20:58:14 +0800

    Update rename_diff.py

--- Model_Generation_and_Evaluation/PostProcess/rename_diff.py
+++ Model_Generation_and_Evaluation/PostProcess/rename_diff.py
@@ -53,8 +53,7 @@ def main():
         try:
             # Get subfolder name
             relative_path = pdb_file.relative_to(input_dir)
-            midfolder_name = relative_path.parent.name if relative_path.parent.name else "root"
-            subfolder_name = midfolder_name.parent.name if midfolder_name.parent.name else "root"
+            subfolder_name = relative_path.parent.parent.name
             
             # Extract the number after the last underscore in the original filename
             original_filename = pdb_file.stem
